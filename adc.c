c696a519c3e2ac3ad2eec6f76db15a2380ce0bb3
jonastoennessen <31915498+jonastoennessen@users.noreply.github.com>
2017-10-04 08:49:46 +0200

    Update adc.c

--- adc.c
+++ adc.c
@@ -2,20 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <avr/io.h>
+#include <util/delay.h>
 
-#include "adc.h"
 #include "memory.h"
+#include "adc.h"
 
 void adc_init(void)
 {
 	memory_init();
 }
 
-uint8_t adc_read(adc_channel channel)
+uint8_t adc_read(uint8_t channel)
 {
+	volatile char *adc = (char *) 0x1400;
+		
 	*adc = channel;
 	
+	_delay_us(300);
+	
 	// Wait for transmitting data from adc to memory
+	while (PINE & (1 << PINE0));
 	
 	return *adc;
 }
