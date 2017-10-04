12cb833d20f851f4ae13798fd7a8fd3f3672542b
jonastoennessen <31915498+jonastoennessen@users.noreply.github.com>
2017-10-04 08:49:18 +0200

    Update adc.h

--- adc.h
+++ adc.h
@@ -2,14 +2,16 @@
 #ifndef ADC_H_
 #define ADC_H_
 
+/*
 typedef enum{
 	joystick_x		= 0,
 	joystick_y		= 1,
 	slider_left		= 2,
 	slider_right    = 3
 	} adc_channel;
+	*/
 
-void adc_init(void)
-uint8_t adc_read(adc_channel channel)
+void adc_init(void);
+uint8_t adc_read(uint8_t channel);
 
 #endif /* ADC_H_ */
